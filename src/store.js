f64acef0c27edcb8e6be2d2e0b853ec9049d3ad1
Jeevan gill <jsg17@hotmail.co.uk>
2019-10-21 21:43:30 -0700

    work without redux being installed

--- src/store.js
+++ src/store.js
@@ -11,7 +11,10 @@ const store = createStore(
     initialState,
     compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        window.REDUX_DEVTOOLS_EXTENSION_COMPOSE ? window.REDUX_DEVTOOLS_EXTENSION_COMPOSE({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+            }) : compose
+            
     )
 )
 
